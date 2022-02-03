80fb91cd5375231ebd312f0e180cc65c1f9f51a1
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-02-03 16:15:20 +0300

    add lesson 1 and 2

---
+++ app/src/main/java/com/svetlana/learn/androidproffcourse/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.svetlana.learn.androidproffcourse.domain
+
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun deleteShopItem(shopItem: ShopItem){
+        shopListRepository.deleteShopItem(shopItem)
+    }
+}
--- app/src/main/java/com/svetlana/learn/androidproffcourse/presentation/MainActivity.kt
+++ app/src/main/java/com/svetlana/learn/androidproffcourse/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.svetlana.learn.androidproffcourse
+package com.svetlana.learn.androidproffcourse.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.svetlana.learn.androidproffcourse.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
