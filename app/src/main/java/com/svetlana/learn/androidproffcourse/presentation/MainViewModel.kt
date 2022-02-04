7f7bd2f3106266e8b9c01334ee7f01d526cd3de7
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-02-04 08:43:51 +0300

    add lesson 4

---
+++ app/src/main/java/com/svetlana/learn/androidproffcourse/presentation/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.svetlana.learn.androidproffcourse.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.svetlana.learn.androidproffcourse.data.ShopListRepositoryImpl
+import com.svetlana.learn.androidproffcourse.domain.*
+
+class MainViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList(){
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
