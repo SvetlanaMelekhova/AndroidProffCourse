cad38b42326850778eeac8b60d3e1ed607cae8a0
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-02-08 11:33:40 +0300

    add lesson 9

---
+++ app/src/main/java/com/svetlana/learn/androidproffcourse/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package com.svetlana.learn.androidproffcourse.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.svetlana.learn.androidproffcourse.R
+
+class ShopItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
